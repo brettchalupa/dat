1c8dbae8712c0ab35aca76a48e307cda81e333fa
Brett Chalupa <brettchalupa@gmail.com>
2015-05-03 16:37:59 -0400

    Add Version and Authors

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "dat"
 	app.Usage = "print the date and time"
+	app.Version = "0.1.0"
+	app.Authors = []cli.Author{cli.Author{Name: "Brett Chalupa", Email: "brett@brettchalupa.com"}}
 	app.Action = func(c *cli.Context) {
 		const layout = "Jan 2, 2006 at 3:04pm (MST)"
 		t := time.Now()
